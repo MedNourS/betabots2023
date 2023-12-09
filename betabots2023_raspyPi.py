6261e8bddfe3d8c965389435d95b32b5e68aad21
MedNourS <sabir.med.nour@gmail.com>
2023-12-09 15:30:34 -0500

    Update betabots2023_raspyPi.py

--- betabots2023_raspyPi.py
+++ betabots2023_raspyPi.py
@@ -16,9 +16,7 @@ import logging
 import ntcore
 
 # Function to detect apriltag and estimate pose
-def pose_estimation(frame, estimator):
-    
-    global tag_id, x, y, yaw
+def pose_estimation(frame, estimator, size):
 
     # Create an AprilTag detector
     detector = rpat.AprilTagDetector()
@@ -36,14 +34,40 @@ def pose_estimation(frame, estimator):
         tag_id = detection.getId()
         x, y = detection.getCenter().x, detection.getCenter().y
 
-        # Print the information
-        print(f"[tag= {tag_id}, x= {x}, y= {y}]")
-
         # Use the pose estimator to estimate the tag's pose
-        pose = estimator.estimate(detection)
+        
+        pose = rpat.AprilTagPoseEstimator(estimator).estimate(detection)
         translation = pose.translation()
         rotation = pose.rotation()
-        roll, pitch, yaw = rotation.x, rotation.y, rotation.z
+        pitch, yaw, roll = rotation.x_degrees, rotation.y_degrees, rotation.z_degrees
+        
+        #TODO distance code below (hopefully)
+        
+        # Get all AprilTag corners
+        pt1 = detection.getCorner(0)
+        pt2 = detection.getCorner(1)
+        pt3 = detection.getCorner(2)
+        pt4 = detection.getCorner(3)
+
+        # Length 1
+        yLength1 = abs(pt1.y - pt4.y)
+        xLength1 = abs(pt1.x - pt4.x)
+        pixelSideLength1 = ((xLength1)**2+(yLength1)**2)**0.5
+
+        # Length 2
+        yLength2 = abs(pt2.y - pt3.y)
+        xLength2 = abs(pt2.x - pt3.x)
+        pixelSideLength2 = ((xLength2)**2 + (yLength2)**2)**0.5
+
+        # Calculate average pixel side length (of length 1 and 2)
+        #// avgSidePixelLength = (pixelSideLength1 + pixelSideLength2)/2
+
+        #tagDistance = (size / avgSidePixelLength)*1000 - 0.55
+        tagDistance = (size * 650) / ((pixelSideLength1 * pixelSideLength2) ** 0.5)
+
+        # Returns the AprilTag information
+        return [tag_id, int(x), int(y), round(pitch, 2), round(yaw, 2), round(roll, 2), round(tagDistance, 2)]
+
 
 # Main function
 def main():
@@ -54,7 +78,7 @@ def main():
     # Initialize NT4 client
     inst = ntcore.NetworkTableInstance.getDefault()
     inst.startClient4("raspyPi")
-    inst.setServer("192.168.0.21") #! Change IP with server's IP
+    inst.setServer("192.168.0.113") #! Change IP with server's IP
 
     # Fetch Table, Topic and Publisher
     table = inst.getTable("9076")
@@ -62,32 +86,26 @@ def main():
 
     # Open the camera (0 is usually the default camera)
     cap = cv2.VideoCapture(0)
-
+    
     # Create an AprilTag pose estimator configuration
     tag_size = 0.1778  # Replace with the actual size of your AprilTag in meters
-    fx, fy, cx, cy = 1000.0, 1000.0, 640.0, 480.0  # Replace with your camera's intrinsic parameters
+    fx, fy, cx, cy = 1000.0, 1000.0, 640.0, 480.0  #! Replace with your camera's intrinsic parameters
     estimator_config = rpat.AprilTagPoseEstimator.Config(tag_size, fx, fy, cx, cy)
-
-    # Create an AprilTag pose estimator with the specified configuration
-    estimator = rpat.AprilTagPoseEstimator(estimator_config)
 
     while True:
         # Capture a frame from the camera
         ret, frame = cap.read()
 
-        # Detect and estimate poses for AprilTags in the captured frame
-        try:
-            publish.set([tag_id, x, y, yaw])
-        except:
-            publish.set([-1,-1,-1,-1])
-
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if not ret:
             break
 
-    # Release the camera and close the OpenCV window
-    cap.release()
-    cv2.destroyAllWindows()
+        poseList = pose_estimation(frame, estimator_config, tag_size)
+
+        # Detect and estimate poses for AprilTags in the captured frame
+        try:
+            publish.set(poseList)
+        except:
+            publish.set([-1, -1, -1, -1, -1, -1, -1])
 
 if __name__ == "__main__":
     main()
